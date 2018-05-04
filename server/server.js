2a07f45aabc91dbf3a8645c41b2f32bd7ddf1ab1
yogitakshirsagar <yogitakshirsagar1112@gmail.comgit config --global user,name yogitakshirsagargit config --global user.email yogitakshirsagar1112@gmail.com>
2018-05-04 18:57:57 +0530

    emit newMessage on CreateMessage

--- server/server.js
+++ server/server.js
@@ -14,24 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-    // from: 'mike@example.com',
-    // text: 'Hey. What is going on.',
-    // createAt: 123
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-    // console.log('createEmail', newEmail);
-  // });
-  
   socket.emit('newMessage', {
-    from: 'Arvind',
-    text: 'Server new message',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
-  
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
