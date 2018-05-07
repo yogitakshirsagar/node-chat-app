4350cbb2249296fabaf9782c086a1b5238b6e651
yogitakshirsagar <yogitakshirsagar1112@gmail.comgit config --global user,name yogitakshirsagargit config --global user.email yogitakshirsagar1112@gmail.com>
2018-05-07 15:29:47 +0530

    Add and show messges form

--- server/server.js
+++ server/server.js
@@ -33,9 +33,10 @@ io.on('connection', (socket) => {
     // createdAt: new Date().getTime()
   // });
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+	callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
