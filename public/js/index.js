26d6e4743b00830ed68713cf87b1c93fe09de3cd
yogitakshirsagar <yogitakshirsagar1112@gmail.comgit config --global user,name yogitakshirsagargit config --global user.email yogitakshirsagar1112@gmail.com>
2018-05-08 15:58:34 +0530

    Add css to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -37,28 +37,33 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'Yogita',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
-  });
+});
   
   var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+  
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
-	  
-	  //console.log(position);
-    socket.emit('createLocationMessage', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
+	     locationButton.removeAttr('disabled').text('Send location');
+		//console.log(position);
+		socket.emit('createLocationMessage', {
+		latitude: position.coords.latitude,
+		longitude: position.coords.longitude
     });
   }, function () {
-    alert('Unable to fetch location.');
+			locationButton.removeAttr('disabled').text('Send location');
+			alert('Unable to fetch location.');
   });
 });
