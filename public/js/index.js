1abf9a683b6d81a6754228cc582ddd9b3d790ee2
yogitakshirsagar <yogitakshirsagar1112@gmail.comgit config --global user,name yogitakshirsagargit config --global user.email yogitakshirsagar1112@gmail.com>
2018-05-08 15:04:42 +0530

    add geoloaction

--- public/js/index.js
+++ public/js/index.js
@@ -17,20 +17,48 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage', {
+  // from: 'Frank',
+  // text: 'Hi'
+// }, function (data) {
+  // console.log('Got it', data);
+// });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   socket.emit('createMessage', {
-    from: 'User',
+    from: 'Yogita',
     text: jQuery('[name=message]').val()
   }, function () {
 
   });
   });
+  
+  var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+	  
+	  //console.log(position);
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
+});
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 
 
 const publicPath = path.join(__dirname, '../public');
@@ -51,6 +51,12 @@ io.on('connection', (socket) => {
       // createdAt: new Date().getTime()
     // });
   // });
+  
+  socket.on('createLocationMessage', (coords) => {
+    //io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+	  io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
